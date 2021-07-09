a6217c86488808dee87a4b3fcd37faf251de3267
MihirS57 <shahmihir507@gmail.com>
2021-07-09 23:02:51 +0530

    Get orders for suppliers and customers

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,6 +1,7 @@
 const Order = require('../models/Order');
 const Product = require('../models/Product');
 const errorResponse = require('../utils/errorResponse');
+
 // @desc    Order product
 // @route   POST /api/v1/orderProduct/:productId
 // @access  Private/User/Customer
@@ -34,3 +35,43 @@ exports.orderProduct = async (req,res,next) => {
         next(new errorResponse(err.message,404));
     }
 }
+
+
+// @desc    Fetch all orders
+// @route   GET /api/v1/orders
+// @access  Private/Supplier/Customer
+exports.getOrders = async (req,res,next) => {
+    try{
+        let orders;
+        if(req.user.role == 'supplier'){
+            orders = await Order.find({
+                supplier: req.user.id
+            });
+
+            if(!orders){
+                return next(new errorResponse('No orders found!',400));
+            }
+
+        }else if(req.user.role == 'customer'){
+            orders = await Order.find({
+                customer: req.user.id
+            });
+
+            if(!orders){
+                return next(new errorResponse('No orders found!',400));
+            }
+        }
+        res.status(200).
+        json({
+            success: true,
+            user: req.user.name,
+            role: req.user.role,
+            count:orders.length,
+            orders: orders
+        });
+    }catch(err){
+        console.log(err.stack)
+        next(new errorResponse(err.message,404));
+    }
+}
+
--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {orderProduct} = require('../controllers/orders');
+const {orderProduct,getOrders} = require('../controllers/orders');
 const {protect,authorize} = require('../middleware/auth');
 
 router.route('/:productId').post(protect,authorize('customer'),orderProduct);
-
+router.route('/orders').get(protect,authorize('supplier','customer'),getOrders);
 module.exports = router;
